87ca1cb05f2c75996070d781a251c9b9c1481d90
IselDrew <andryshonochek0604@gmail.com>
2020-03-15 17:56:19 +0200

    Add animals cards

--- app/components/Animals/ViewAnimals.js
+++ app/components/Animals/ViewAnimals.js
@@ -1,11 +1,47 @@
 export class ViewAnimals {
-    constructor() {
+  constructor() {
+      this.animalsContainer = document.querySelector('.row');
+  }
 
-    }
+  getSingleAnimal(animal) {
+    console.log(animal);
 
-    renderAnimals(arr){
-        console.log(arr);
-        // document.body.innerHTML = `<img src='data:image/jpg;base64,${arr[1].image}'/>`
-    }
+    return ` 
+        <div class="col-lg-4 col-md-12 mb-4">
+            <div class="card">
+              <div class="view view-cascade overlay">
+                <img src="data:image/jpg;base64,${animal.image}" class="card-img-top"
+                  alt="sample photo">
+                <a>
+                  <div class="mask rgba-white-slight"></div>
+                </a>
+              </div>
+
+              <div class="card-body">
+                <h4 class="card-title indigo-text text-capitalize">
+                  ${animal.breed}
+                  <p class="mb-1 grey-text"><small>${animal.gender} ${animal.species}</small></p>
+                </h4>
+                <p class="card-text">
+                  ${animal.birth_date}
+                </p>
+                
+
+                <p class="mb-1"><strong>$${animal.price}</strong></p>
+
+                <button type="button" class="btn btn-indigo btn-rounded btn-sm px-3">Add to cart</button>
+                <button type="button" class="btn btn-outline-indigo btn-rounded btn-sm px-3 waves-effect">Details</button>
+              </div>
+            </div>
+          </div>
+        `;
+  }
+
+  getAnimals(animals) {
+    return animals.map(animal => this.getSingleAnimal(animal)).join('')
+  }
+
+  renderAnimals(animals) {
+    this.animalsContainer.innerHTML = this.getAnimals(animals);
+  }
 }
-
