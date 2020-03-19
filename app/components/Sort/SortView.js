1f31fdf3e82be7783808eb57ddfefc37038f0aa3
IselDrew <andryshonochek0604@gmail.com>
2020-03-19 17:20:36 +0200

    Change sort listener

--- app/components/Sort/SortView.js
+++ app/components/Sort/SortView.js
@@ -2,8 +2,12 @@ export class SortView {
   constructor(cbSort) {
     this.sortItems = document.querySelector(".sort-items");
     this.sortItems.addEventListener("click", event => {
-      const value = event.target.attributes["data-id"].value;
+      const value = this.getSortValue(event);
       cbSort(value);
     });
+  }
+
+  getSortValue(event) {
+    return event.target.dataset.id;
   }
 }
