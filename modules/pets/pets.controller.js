ef723340049f8859a265644fb9a05a36158587ed
IselDrew <andryshonochek0604@gmail.com>
2020-03-29 18:22:34 +0300

    Parse query-string to object for db-request

--- modules/pets/pets.controller.js
+++ modules/pets/pets.controller.js
@@ -5,65 +5,63 @@ const { Op } = require("sequelize");
 // const data = require('../data/data.json');
 
 class PetsController {
-  // async getPets(req, res, next) {
-  //     try {
-  //         const
-  //     } catch {
-
-  //     }
-  // }
-
-  async findMany(req, res, next) {
+  async getPets(req, res, next) {
     try {
       const specificReq = req.query;
 
       const order = [];
       const where = {};
 
+      const fullRequest = {
+        limit: parseInt(specificReq.limit) || 9,
+        offset: parseInt(specificReq.offset) || 0
+      };
+
       // resolving query string for Sort
       if (specificReq.sortType) {
         switch (specificReq.sortType) {
           case "priceUp": {
             order.push(["price", "ASC"]);
+            break;
           }
           case "priceDown": {
             order.push(["price", "DESC"]);
+            break;
           }
           case "ageUp": {
             order.push(["birth_date", "ASC"]);
+            break;
           }
           case "ageDown": {
             order.push(["birth_date", "DESC"]);
+            break;
           }
         }
+        fullRequest.order = order;
       }
 
       // resolving query string for Filter
-      if (specificReq.filter) {
-        where.species = specificReq.filter;
+      if (specificReq.filterType) {
+        where.species = specificReq.filterType;
+        fullRequest.where = where;
       }
 
       // resolving query string for Search
-      if (specificReq.search) {
+      if (specificReq.searchString) {
         where.breed = {
-          [Op.like]: `%${specificReq.search}%`
+          [Op.like]: `%${specificReq.searchString}%`
         };
+        fullRequest.where = where;
       }
 
-      // resolving query string for Pagination
-      const pagination = {
-        limit: parseInt(specificReq.limit) || 3,
-        offset: parseInt(specificReq.offset) || 0
-      };
-
-      const pets = await petsService.findMany();
+      const pets = await petsService.findMany(fullRequest);
       res.json(pets);
     } catch (e) {
       next(e);
     }
   }
 
-  async findOneById(req, res, next) {
+  async findPetById(req, res, next) {
     try {
       const pet = await petsService.findOneById(req.params.id);
       res.json(pet);
